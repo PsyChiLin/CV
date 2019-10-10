8eee52260fb983885f3a8e7bb11a68cded5ab8bb
PsyChiLin <psychilinyu@gmail.com>
2019-10-10 09:13:33 -0400

    CJP

---
+++ Submitted_Review_Inpre.R
@@ -0,0 +1,11 @@
+## Journal Articles : Under Review/Revision
+
+**Yu, C. L.**, Chen, H. C., Yang, Z. Y., & Chou, T. L. “Multi-Time Points Analysis: A Time Course Analysis with Functional Near-Infrared Spectroscopy”
+
+## Journal Articles : In Preparation
+
+**Yu, C. L.**, Lee, S. H., Gau S., & Chou, T. L. “Neural Correlates of Theory of Mind in Typically Developing Youths and Youths with Autism Spectrum Disorder”
+
+**Yu, C. L.**, Causeur D., Shen, I. H., & Sheu, C. F. “ggerp: Visualization of Event-Related Brain Potentials with ggplot2”
+
+Hu, J. F., **Yu, C. L.**, Tsai Y. L., Hsiao, Y. R. & Chen, H. C. “The Role of Theory of Mind Ability in Verbal Humor Comprehension: A functional magnetic resonance imaging study” (*As the corresponding author*)
---
+++ guideline.R
@@ -0,0 +1,15 @@
+1. ED
+
+2. [Awards, Fellowshipd] Rackham 
+
+3. Research Interests
+
+4. Publication / Submited (number)
+
+5. Presentation
+
+6. [Edinorial contributions] (co-reviewer)
+
+7. Professional Activities (Student members)
+
+8. References/Email
