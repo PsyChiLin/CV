d5f97d1f3a14d0e8fab56011f90c459e11cbea9b
PsyChiLin <psychilinyu@gmail.com>
2019-12-30 14:45:28 -0500

    Jean; BRM

--- Submitted_Review_Inpre.R
+++ Submitted_Review_Inpre.R
@@ -1,7 +1,3 @@
-## Journal Articles : Under Review/Revision
-
-**Yu, C. L.**, Chen, H. C., Yang, Z. Y., & Chou, T. L. “Multi-Time Points Analysis: A Time Course Analysis with Functional Near-Infrared Spectroscopy”
-
 ## Journal Articles : In Preparation
 
 **Yu, C. L.**, Lee, S. H., Gau S., & Chou, T. L. “Neural Correlates of Theory of Mind in Typically Developing Youths and Youths with Autism Spectrum Disorder”
