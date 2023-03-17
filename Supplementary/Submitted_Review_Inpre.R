04c1c48d33cf42079afb8bbebac1d8cf014ed020
Chi-Lin Yu <psychilinyu@gmail.com>
2023-03-17 11:47:09 -0400

    hiring

--- Supplementary/Submitted_Review_Inpre.R
+++
@@ -1,7 +0,0 @@
-## Journal Articles : In Preparation
-
-**Yu, C. L.**, Lee, S. H., Gau S., & Chou, T. L. “Neural Correlates of Theory of Mind in Typically Developing Youths and Youths with Autism Spectrum Disorder”
-
-**Yu, C. L.**, Causeur D., Shen, I. H., & Sheu, C. F. “ggerp: Visualization of Event-Related Brain Potentials with ggplot2”
-
-Hu, J. F., **Yu, C. L.**, Tsai Y. L., Hsiao, Y. R. & Chen, H. C. “The Role of Theory of Mind Ability in Verbal Humor Comprehension: A functional magnetic resonance imaging study” (*As the corresponding author*)
